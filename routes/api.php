f2d87325d0ad56ec2c4d155614c492d696ff75fe
Dzul Maarij <fathesshop@gmail.com>
2021-02-08 20:41:45 +0700

    pospondokprogrammer

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,13 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Auth::routes(['verify' => true]);
 Route::post('login', 'Api\UserController@login');
 Route::post('register', 'Api\UserController@register');
 Route::post('password/email', 'Api\ForgotPasswordController@sendResetLinkEmail');
 Route::post('password/reset', 'Api\ResetPasswordController@reset');
-Route::get('/email/resend', 'Api\VerificationController@resend');
-Route::get('/email/verify/{id}/{hash}', 'Api/VerificationController@verify');
+Route::get('/email/resend', 'Api\VerificationController@resend')->name('verification.resend');;
+Route::get('/email/verify/{id}/{hash}', 'Api\VerificationController@verify')->name('verification.verify');
 
 
 Route::group(['middleware' => 'auth:api', 'namespace' => 'Api'], function () {
