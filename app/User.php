485b6ba7f847bc84f114112618a49e39a87cb383
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-02-09 14:50:53 +0700

    pospondokprogrammer

--- app/User.php
+++ app/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'token', 'umur', 'alamat'
+        'nama', 'email', 'password', 'token', 'umur', 'alamat'
     ];
 
     /**
